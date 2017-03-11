8250712494e25015ec4493a4b82ebf23bbf72967
久保田公平 <kubotakouhei@kubotakouhei-no-MacBook-Pro.local>
2017-03-11 16:40:46 +0900

    [repair] sort

--- median.py
+++ median.py
@@ -1,24 +1,29 @@
 def median(*args):
-    arr = []
-    for i in args:
-        arr.append(i)
+    arr = list(args)
     k = 0
     w = 0
     i = len(arr) - 1
     index_med = round(i/2)
-    while k < i:
-        while True:
-            if k <= i:
-                if arr[i] < arr[i-1]:
-                    w = arr[i-1]
-                    arr[i-1] = arr[i]
-                    arr[i] = w
+    while k < len(arr) - 1:
+        while k < i:
+            if arr[i] < arr[i-1]:
+                w = arr[i-1]
+                arr[i-1] = arr[i]
+                arr[i] = w
                 i = i - 1
+                if k >= i:
+                    k = k + 1
+                    i = len(arr) - 1
             else:
-                k = k + 1
-                break
+                i = i - 1
+                if k >= i:
+                    k = k + 1
+                    i = len(arr) - 1
     return arr[index_med]
 
 print(median(5,7,2))
 print(median(7,3,2))
 print(median(1,4,2))
+print(median(4,2,1))
+print(median(1,2,4))
+print(median(3,6,4,32,21,7,10))
