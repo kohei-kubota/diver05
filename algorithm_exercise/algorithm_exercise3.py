698f7625eea9be4ba4ade6447e32fb126b77e66e
久保田公平 <kubotakouhei@kubotakouhei-no-MacBook-Pro.local>
2017-03-11 17:39:20 +0900

    [create] algorithm3

---
+++ algorithm_exercise/algorithm_exercise3.py
@@ -0,0 +1,30 @@
+def sort(*args):
+    arr = []
+    for j in args:
+        for l in j:
+            arr.append(l)
+    k = 0
+    w = 0
+    i = len(arr) - 1
+    index_med = round(i/2)
+    while k < len(arr) - 1:
+        while k < i:
+            if arr[i-1] < arr[i]:
+                w = arr[i]
+                arr[i] = arr[i-1]
+                arr[i-1] = w
+                i = i - 1
+                if k >= i:
+                    k = k + 1
+                    i = len(arr) - 1
+            else:
+                i = i - 1
+                if k >= i:
+                    k = k + 1
+                    i = len(arr) - 1
+    return arr
+
+sample_list = ['a', 'b', 'c']
+print(sort(sample_list))
+sample_list = [1, 2, 3]
+print(sort(sample_list))
